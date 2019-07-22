b5d643beca789d00296c62a2322b2f04100ca9f4
Paulo Augusto Nogueira Foroni <paulo.foroni@linx.com.br>
2019-07-22 16:23:42 -0300

    first commit

---
+++ node-api/src/routes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const routes = express.Router();
+
+const ProductController = require('./controllers/ProductController');
+
+routes.get('/products', ProductController.index);
+routes.get('/products/:id', ProductController.show);
+routes.post('/products', ProductController.store);
+routes.put('/products/:id', ProductController.update);
+routes.delete('/products/:id', ProductController.destroy);
+
+
+/*routes.get('/products', (req, res) => {
+    Product.create({
+        title: "React Native",
+        description: "Build native apps with React",
+        url: "http://github.com/facebook/react-native"
+    })
+
+    return res.send("hello world!");
+});*/
+
+module.exports = routes;
